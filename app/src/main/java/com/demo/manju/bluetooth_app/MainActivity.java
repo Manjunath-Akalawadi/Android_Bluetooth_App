3ed250ad4fb767aa63377fced7eaf8249efe52db
Manjunath-Akalawadi <manjuakalawadi@gmail.com>
2018-08-27 21:42:08 +0530

    wifi added

--- app/src/main/java/com/demo/manju/bluetooth_app/MainActivity.java
+++ app/src/main/java/com/demo/manju/bluetooth_app/MainActivity.java
@@ -1,17 +1,20 @@
 package com.demo.manju.bluetooth_app;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button bON , bOFF , dView;
+    Button bON , bOFF , dView , wON , wOFF;
     BluetoothAdapter bluetoothAdapter;
+
     Intent intentEnable;
     int requestCode;
 
@@ -23,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
         bON = (Button) findViewById(R.id.bOn);
         bOFF = (Button) findViewById(R.id.bOff);
         dView = (Button) findViewById(R.id.viewD);
+        wON = (Button) findViewById(R.id.wOn);
+        wOFF = (Button) findViewById(R.id.wOff);
 
 
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -31,9 +36,35 @@ public class MainActivity extends AppCompatActivity {
         
         bluetoothOn();
         bluetoothOff();
+        wifiOn();
+        wifiOff();
         viewDevices();
 
 
+    }
+
+    private void wifiOn() {
+
+            wON.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+                    wifiManager.setWifiEnabled(true);
+                    Toast.makeText(getApplicationContext(),"Wifi is Enabled",Toast.LENGTH_LONG).show();
+                }
+            });
+    }
+
+    private void wifiOff() {
+
+        wOFF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+                wifiManager.setWifiEnabled(false);
+                Toast.makeText(getApplicationContext(),"Wifi is Disabled",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
